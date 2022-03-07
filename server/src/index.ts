593f677b196ab7736668f450a561d86ca67d234d
Rei-Sogawa <natto.and.eggs@gmail.com>
2022-03-07 21:31:29 +0900

    fix

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,10 +3,7 @@ import express from "express";
 import { readFileSync } from "fs";
 import expressPlayground from "graphql-playground-middleware-express";
 
-import { getDb } from "./firebase-app";
-import { FirestoreDataSource } from "./lib/datasource/datasource";
-import { Converter } from "./lib/datasource/helper";
-import { IUser } from "./lib/entity/user";
+import { dataSources } from "./lib/datasource/index";
 import { resolvers } from "./resolvers";
 
 const typeDefs = readFileSync("./typeDefs.graphql", "utf-8");
@@ -17,15 +14,7 @@ async function start() {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    dataSources: () => {
-      console.log("--- in dataSources initialized ---");
-      const db = getDb();
-      const usersRef = db.collection("users").withConverter(Converter<IUser>());
-      class UserApi extends FirestoreDataSource<IUser> {}
-      return {
-        userApi: new UserApi(usersRef),
-      };
-    },
+    dataSources,
     context: () => {
       console.log("--- context initialized ---");
       return {};
--- server/src/lib/datasource/index.ts
+++ server/src/lib/datasource/index.ts
@@ -1,1 +1,17 @@
-export { FirestoreDataSource } from "./datasource";
+import { getDb } from "../../firebase-app";
+import { IUser } from "../entity/user";
+import { FirestoreDataSource } from "./firestore-datasource";
+import { Converter } from "./firestore-datasource/helper";
+
+const db = getDb();
+
+export const usersRef = db.collection("users").withConverter(Converter<IUser>());
+
+export class Users extends FirestoreDataSource<IUser> {}
+
+export const dataSources = () => {
+  console.log("--- dataSources initialized ---");
+  return {
+    users: new Users(usersRef),
+  };
+};
