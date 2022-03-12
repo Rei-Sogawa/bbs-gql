b7d4e7c6c7eacf7d56afbc7e1ff37c29a813e567
Rei-Sogawa <natto.and.eggs@gmail.com>
2022-03-12 13:00:18 +0900

    fix

--- server/src/lib/entity/user.ts
+++ server/src/lib/entity/user.ts
@@ -1,25 +1,5 @@
 import * as admin from "firebase-admin";
-import { v4 } from "uuid";
 import { z } from "zod";
-
-export const UserSchema = z.object({
-  id: z.string().uuid(),
-  createdAt: z.instanceof(admin.firestore.Timestamp),
-  updatedAt: z.instanceof(admin.firestore.Timestamp),
-});
-
-export type IUser = z.infer<typeof UserSchema>;
-
-export class User {
-  static of(value: Partial<IUser> = {}) {
-    return {
-      id: v4(),
-      createdAt: admin.firestore.Timestamp.now(),
-      updatedAt: admin.firestore.Timestamp.now(),
-      ...value,
-    };
-  }
-}
 
 export const UserDataSchema = z.object({
   createdAt: z.instanceof(admin.firestore.Timestamp).default(admin.firestore.Timestamp.now()),
