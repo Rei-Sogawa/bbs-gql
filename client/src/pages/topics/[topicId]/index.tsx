b7dde64dcadc4e1ced647418bd2cec19b0b2b5d0
Rei-Sogawa <natto.and.eggs@gmail.com>
2022-03-13 15:34:11 +0900

    fix

--- client/src/pages/topics/[topicId]/index.tsx
+++ client/src/pages/topics/[topicId]/index.tsx
@@ -1,9 +1,11 @@
 import { gql } from "@apollo/client";
 import { format } from "date-fns";
+import { FaEllipsisV } from "react-icons/fa";
 import { Link, useParams } from "react-router-dom";
 
 import { AppHeading } from "../../../components/AppHeading";
 import { AppLayout } from "../../../components/AppLayout";
+import { useAuth } from "../../../contexts/Auth";
 import { TopicForTopicDetailFragment, useTopicQuery } from "../../../graphql/generated";
 import { routes } from "../../../routes";
 
@@ -25,15 +27,39 @@ type TopicDetailProps = {
 };
 
 const TopicDetail = ({ topic }: TopicDetailProps) => {
+  const { uid } = useAuth();
+
   return (
-    <div className="card border">
-      <div className="card-body">
-        <AppHeading>{topic.title}</AppHeading>
-        <div className="flex space-x-4 items-center">
-          <div className="font-bold">{topic.user.displayName}</div>
-          <div className="text-gray-500 text-sm">{format(new Date(topic.createdAt), "yyyy/MM/dd HH:mm")}</div>
+    <div className="p-4 border rounded-md">
+      <div className="flex flex-col space-y-4">
+        <div className="flex justify-between items-center">
+          <AppHeading>{topic.title}</AppHeading>
+          {topic.user.id === uid && (
+            <div>
+              <div className="dropdown dropdown-end">
+                <label tabIndex={0} className="btn btn-ghost btn-xs border-gray-200">
+                  <FaEllipsisV />
+                </label>
+                <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+                  <li>
+                    <button className="btn btn-ghost">Edit</button>
+                  </li>
+                  <li>
+                    <button className="btn btn-ghost">Delete</button>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          )}
         </div>
-        <div>{topic.description}</div>
+
+        <div>
+          <div className="flex space-x-4 items-center">
+            <div className="font-bold">{topic.user.displayName}</div>
+            <div className="text-gray-500 text-sm">{format(new Date(topic.createdAt), "yyyy/MM/dd HH:mm")}</div>
+          </div>
+          <div>{topic.description}</div>
+        </div>
       </div>
     </div>
   );
