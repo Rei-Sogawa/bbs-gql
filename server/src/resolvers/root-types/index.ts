01b84d0bc59302efb305fe96efd267a5b052a18b
Rei-Sogawa <natto.and.eggs@gmail.com>
2022-03-15 00:18:24 +0900

    fix

--- server/src/resolvers/root-types/index.ts
+++ server/src/resolvers/root-types/index.ts
@@ -4,6 +4,8 @@ export const Topic: Resolvers["Topic"] = {
   user: async (parent, _args, context) => {
     const { userId } = parent;
     const { userRepository } = context.repositories;
-    return userRepository.findById(userId);
+    const userRaw = await userRepository.findById(userId);
+    if (!userRaw) throw new Error("Not found");
+    return userRaw;
   },
 };
