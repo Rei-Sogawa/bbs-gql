50aba17997b57a13d87bc4b3cc81e551b42b8fd1
Rei-Sogawa <natto.and.eggs@gmail.com>
2022-03-15 21:52:22 +0900

    fix

--- client/src/pages/topics/[topicId]/index.tsx
+++ client/src/pages/topics/[topicId]/index.tsx
@@ -6,9 +6,45 @@ import { Link, useNavigate, useParams } from "react-router-dom";
 import { AppHeading } from "../../../components/AppHeading";
 import { AppLayout } from "../../../components/AppLayout";
 import { useAuth } from "../../../contexts/Auth";
-import { TopicForTopicDetailFragment } from "../../../graphql/generated";
+import { Topic as ITopic, TopicForTopicDetailFragment } from "../../../graphql/generated";
 import { useDeleteTopic, useTopic } from "../../../hooks/useTopics";
 import { routes } from "../../../routes";
+
+const TopicMenu = (props: { topic: Pick<ITopic, "id"> }) => {
+  const navigate = useNavigate();
+
+  const deleteTopic = useDeleteTopic();
+  const onDelete = async () => {
+    await deleteTopic({ variables: { id: props.topic.id } });
+    navigate(routes["/"].path());
+  };
+
+  const onEdit = () => {
+    navigate(routes["/topics/:topicId/edit"].path({ topicId: props.topic.id }));
+  };
+
+  return (
+    <div>
+      <div className="dropdown dropdown-end">
+        <label tabIndex={0} className="btn btn-ghost btn-xs border-gray-200">
+          <FaEllipsisV />
+        </label>
+        <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+          <li>
+            <button className="btn btn-ghost" onClick={onEdit}>
+              Edit
+            </button>
+          </li>
+          <li>
+            <button className="btn btn-ghost" onClick={onDelete}>
+              Delete
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 gql`
   fragment TopicForTopicDetail on Topic {
@@ -28,55 +64,22 @@ type TopicDetailProps = {
 };
 
 const TopicDetail = ({ topic }: TopicDetailProps) => {
-  const navigate = useNavigate();
-
   const { uid } = useAuth();
-
-  const deleteTopic = useDeleteTopic();
-  const onDelete = async () => {
-    await deleteTopic({ variables: { id: topic.id } });
-    navigate(routes["/"].path());
-  };
-
-  const onEdit = () => {
-    navigate(routes["/topics/:topicId/edit"].path({ topicId: topic.id }));
-  };
 
   return (
     <div className="p-4 border rounded-md">
-      <div className="flex flex-col space-y-4">
+      <div className="flex flex-col space-y-2">
         <div className="flex justify-between items-center">
           <AppHeading>{topic.title}</AppHeading>
-          {topic.user.id === uid && (
-            <div>
-              <div className="dropdown dropdown-end">
-                <label tabIndex={0} className="btn btn-ghost btn-xs border-gray-200">
-                  <FaEllipsisV />
-                </label>
-                <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
-                  <li>
-                    <button className="btn btn-ghost" onClick={onEdit}>
-                      Edit
-                    </button>
-                  </li>
-                  <li>
-                    <button className="btn btn-ghost" onClick={onDelete}>
-                      Delete
-                    </button>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          )}
+          {topic.user.id === uid && <TopicMenu topic={topic} />}
         </div>
 
-        <div>
-          <div className="flex space-x-4 items-center">
-            <div className="font-bold">{topic.user.displayName}</div>
-            <div className="text-gray-500 text-sm">{format(new Date(topic.createdAt), "yyyy/MM/dd HH:mm")}</div>
-          </div>
-          <div>{topic.description}</div>
+        <div className="flex space-x-4 items-center">
+          <div className="font-bold">{topic.user.displayName}</div>
+          <div className="text-gray-500 text-sm">{format(new Date(topic.createdAt), "yyyy/MM/dd HH:mm")}</div>
         </div>
+
+        <div className="whitespace-pre-line">{topic.description}</div>
       </div>
     </div>
   );
