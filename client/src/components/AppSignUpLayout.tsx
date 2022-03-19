b26d6c4b83af2bc81bb8d4474f4f618b63d9254a
Rei-Sogawa <natto.and.eggs@gmail.com>
2022-03-19 17:54:13 +0900

    fix

--- client/src/components/AppSignUpLayout.tsx
+++ client/src/components/AppSignUpLayout.tsx
@@ -8,7 +8,7 @@ export const AppSignUpLayout: VFC<AppSignUpLayoutProps> = ({ children }) => {
   return (
     <div className="bg-gray-100 h-screen">
       <div className="h-full md:h-auto md:pt-20 bg-gray-100">
-        <div className="md:w-screen-xs h-full md:h-auto mx-auto px-8 py-4 rounded-md bg-white">{children}</div>
+        <div className="md:w-screen-xs h-full md:h-auto mx-auto px-8 py-4 md:rounded-md bg-white">{children}</div>
       </div>
     </div>
   );
