531b8bc72ce7135ae632da395fcd85a04c61ca9c
Rei-Sogawa <natto.and.eggs@gmail.com>
2022-03-19 19:05:13 +0900

    fix

--- server/src/lib/repository/helper/createRepository.ts
+++ server/src/lib/repository/helper/createRepository.ts
@@ -1,29 +1,29 @@
 import * as admin from "firebase-admin";
 
-import { createRootCollectionLoader } from "./createLoader";
+import { createRootCollectionLoader, createSubCollectionLoader } from "./createLoader";
 
-export const createRootCollectionRepository = <Entity extends { id: string }>(
-  ref: admin.firestore.CollectionReference<Entity>
+export const createRootCollectionRepository = <TEntity extends { id: string }>(
+  ref: admin.firestore.CollectionReference<TEntity>
 ) => {
-  const loader = createRootCollectionLoader<Entity>(ref);
+  const loader = createRootCollectionLoader<TEntity>(ref);
 
   const _get = (id: string) => loader.load(id);
 
-  const _set = (entity: Entity) =>
+  const _set = (entity: TEntity) =>
     ref
       .doc(entity.id)
       .set(entity)
       .then(() => entity);
 
-  const _add = (entity: Entity) => ref.add(entity).then(({ id }) => ({ ...entity, id }));
+  const _add = (entity: TEntity) => ref.add(entity).then(({ id }) => ({ ...entity, id }));
 
-  const _update = (entity: Entity) =>
+  const _update = (entity: TEntity) =>
     ref
       .doc(entity.id)
       .update(entity)
       .then(() => entity);
 
-  const _delete = (entity: Entity) =>
+  const _delete = (entity: TEntity) =>
     ref
       .doc(entity.id)
       .delete()
@@ -34,6 +34,47 @@ export const createRootCollectionRepository = <Entity extends { id: string }>(
     loader,
     get: _get,
     set: _set,
+    add: _add,
+    update: _update,
+    delete: _delete,
+  };
+};
+
+export const createSubCollectionRepository = <TEntity extends { id: string }, Key extends { id: string }>(
+  ref: (params: Omit<Key, "id">) => admin.firestore.CollectionReference<TEntity>
+) => {
+  const loader = createSubCollectionLoader(ref);
+
+  const _get = (params: Key) => loader.load(params);
+
+  const _set = (params: Omit<Key, "id">, entity: TEntity) =>
+    ref(params)
+      .doc(entity.id)
+      .set(entity)
+      .then(() => entity);
+
+  const _add = (params: Omit<Key, "id">, entity: TEntity) =>
+    ref(params)
+      .add(entity)
+      .then(({ id }) => ({ ...entity, id }));
+
+  const _update = (params: Omit<Key, "id">, entity: TEntity) =>
+    ref(params)
+      .doc(entity.id)
+      .update(entity)
+      .then(() => entity);
+
+  const _delete = (params: Omit<Key, "id">, entity: TEntity) =>
+    ref(params)
+      .doc(entity.id)
+      .delete()
+      .then(() => entity);
+
+  return {
+    ref,
+    loader,
+    set: _set,
+    get: _get,
     add: _add,
     update: _update,
     delete: _delete,
--- server/src/lib/repository/index.ts
+++ server/src/lib/repository/index.ts
@@ -1,11 +1,14 @@
 import * as admin from "firebase-admin";
 
+import { IComment } from "../entity/comment";
 import { ITopic } from "../entity/topic";
 import { IUser } from "../entity/user";
-import { createRootCollectionRepository } from "./../repository/helper/createRepository";
+import { createRootCollectionRepository, createSubCollectionRepository } from "./../repository/helper/createRepository";
 import { createTimestampConverter } from "./helper/createConverter";
 
-const createTopicRepository = (ref: admin.firestore.CollectionReference<ITopic>) => {
+type CollectionRef<T> = admin.firestore.CollectionReference<T>;
+
+const createTopicRepository = (ref: CollectionRef<ITopic>) => {
   const repository = createRootCollectionRepository(ref);
   return {
     ...repository,
@@ -17,14 +20,36 @@ const createTopicRepository = (ref: admin.firestore.CollectionReference<ITopic>)
   };
 };
 
+const createCommentRepository = (ref: ({ topicId }: { topicId: string }) => CollectionRef<IComment>) => {
+  const repository = createSubCollectionRepository<IComment, { topicId: string; id: string }>(ref);
+  return {
+    ...repository,
+    findAll: ({ topicId }: { topicId: string }) =>
+      ref({ topicId })
+        .orderBy("createdAt", "desc")
+        .get()
+        .then((snap) => snap.docs.map((doc) => doc.data())),
+  };
+};
+
 export const createRepositories = (db: admin.firestore.Firestore) => {
   const usersRef = db.collection("users").withConverter(createTimestampConverter<IUser>());
   const topicsRef = db.collection("topics").withConverter(createTimestampConverter<ITopic>());
 
+  const commentsRef = ({ topicId }: { topicId: string }) => {
+    return db
+      .collection("topics")
+      .doc(topicId)
+      .collection("comments")
+      .withConverter(createTimestampConverter<IComment>());
+  };
+
   const UserRepository = createRootCollectionRepository(usersRef);
   const TopicRepository = createTopicRepository(topicsRef);
 
-  return { UserRepository, TopicRepository };
+  const CommentRepository = createCommentRepository(commentsRef);
+
+  return { UserRepository, TopicRepository, CommentRepository };
 };
 
 export type Repositories = ReturnType<typeof createRepositories>;
